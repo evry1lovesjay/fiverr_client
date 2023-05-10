c79998232e6b1cf2e5f40a3ffb39e8a88152e475
evry1lovesjay <evry1lovesjay@yahoo.com>
2023-05-10 11:27:44 +0100

    changed localhost to render in axios request

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const newRequest = axios.create({
-    baseURL: "http://localhost:9900/api/",
-    withCredentials: true,
-})
-
 // const newRequest = axios.create({
-//     baseURL: "https://fiverr-api-g2gy.onrender.com/api/",
+//     baseURL: "http://localhost:9900/api/",
 //     withCredentials: true,
 // })
+
+const newRequest = axios.create({
+    baseURL: "https://fiverr-api-g2gy.onrender.com/api/",
+    withCredentials: true,
+})
 
 export default newRequest
