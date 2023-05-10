f8aaf0ab7e5255e467ea48964b53846211993d5a
evry1lovesjay <evry1lovesjay@yahoo.com>
2023-05-10 11:09:27 +0100

    added custom carousel/slider

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,9 +2,9 @@ import Featured from "../../components/featured/Featured";
 import Slide from "../../components/slide/Slide";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
 import { cards, projects } from "../../data";
-import CatCard from "../../components/catCard/catCard";
+import CatCard from "../../components/catCard/CatCard";
 import "./Home.scss";
-import ProjectCard from "../../components/projectCard/projectCard";
+import ProjectCard from "../../components/projectCard/ProjectCard";
 
 const Home = () => {
 
