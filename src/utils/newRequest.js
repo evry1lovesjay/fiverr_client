62459c30b91da3ebc516ddff6bbb54f9f6cd0393
evry1lovesjay <evry1lovesjay@yahoo.com>
2023-06-01 13:40:19 +0200

    added prod & dev

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,12 +1,23 @@
 import axios from "axios";
 
+
+// const API_URL = import.meta.env.DEV
+// ? 'http://localhost:3000'
+// : 'https://www.daytrit.com';
+
+
 // const newRequest = axios.create({
 //     baseURL: "http://localhost:9900/api/",
 //     withCredentials: true,
 // })
 
+const API_URL = import.meta.env.DEV
+    ? import.meta.env.VITE_API_URL_DEV
+    : import.meta.env.VITE_API_URL_PROD;
+
+
 const newRequest = axios.create({
-    baseURL: "https://fiverr-api-g2gy.onrender.com/api/",
+    baseURL: `${API_URL}/api/`,
     withCredentials: true,
 })
 
